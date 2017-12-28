a68cfd2ec0e54a3b9e3ed621e9648ac43fe7f182
intern03 <thuy.nguyen@groovetechnology.com>
2017-12-28 13:21:43 +0700

    Test-out can 'an obj lay behind an another'. Yup

--- index.vr.js
+++ index.vr.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   AppRegistry,
   asset,
@@ -6,33 +6,34 @@ import {
   Text,
   View,
   VrButton,
-  Animated
-} from 'react-vr';
+  Animated,
+  Image
+} from "react-vr";
 // import BouncingText from './componet/BouncingText';
 
 export default class side_test extends React.Component {
   render() {
-    return(
-        <View>
-          <Pano source={asset("chess-world.jpg")} />
-          <View
-            style={{
-              flex: 1,
-              width: 2,
-              flexDirection: 'column',
-              alignItems: 'stretch',
-              layoutOrigin: [0.5, 0.5],
-              transform: [{translate: [0, 0, -5]}]
-            }}
-          >
-          <BouncingText message="abcdef"/>
-          </View>
+    return (
+      <View>
+        <Pano source={asset("chess-world.jpg")} />
+        <View
+          style={{
+            flex: 1,
+            width: 2,
+            flexDirection: "column",
+            alignItems: "stretch",
+            layoutOrigin: [0.5, 0.5],
+            transform: [{ translate: [0, 0, -5] }]
+          }}
+        >
+          <TextBoxs />
         </View>
+      </View>
     );
   }
-};
+}
 
-AppRegistry.registerComponent('side_test', () => side_test);
+AppRegistry.registerComponent("side_test", () => side_test);
 class BouncingText extends React.Component {
   constructor(props) {
     super(props);
@@ -67,32 +68,68 @@ class BouncingText extends React.Component {
 }
 class TextBoxs extends React.Component {
   render() {
-    const US_states = { Arizona: "Arizona", NewHampshire: "New Hampshire", California: "California", Hawaii: "Hawaii", Texas: "Texas" };
-    return <View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: "#CF3C7E" }}>
-          <Text style={{ fontSize: 0.2, textAlign: "center" }}>
-            Arizona
-          </Text>
+    const US_states = {
+      Arizona: "Arizona",
+      NewHampshire: "New Hampshire",
+      California: "California",
+      Hawaii: "Hawaii",
+      Texas: "Texas"
+    };
+    return (
+      <View>
+        <View
+          style={
+            {
+              // margin: 0.1,
+              height: 5.25,
+              width: 3.5,
+              backgroundColor: "#CF3C7E",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center"
+            }
+            // flexDirection: "column"
+          }
+        >
+          <Image
+            style={{
+              overflow: "hidden",
+              flex: 0.5,
+              backgroundColor: "#00000080",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+            source={asset("J---HR_Page-6st-strip-green-hair (2).png")}
+          >
+            <View
+              style={{
+                backgroundColor: "#000"
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 0.2,
+                  textAlign: "center",
+                  opacity: 1,
+                  textAlignVertical: "center"
+                }}
+              >
+                Arizona
+              </Text>
+            </View>
+          </Image>
+          <View style={{
+            flex:1,
+            backgroundColor:'#b8860b'
+          }}>
+          <Text>this is a text</Text>
+          </View>
         </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: "#CF3C7E" }}>
-          <Text style={{ fontSize: 0.2, textAlign: "center" }}>
-            New Hampshire
-          </Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: "#CF3C7E" }}>
-          <Text style={{ fontSize: 0.2, textAlign: "center" }}>
-            California
-          </Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: "#CF3C7E" }}>
-          <Text style={{ fontSize: 0.2, textAlign: "center" }}>Hawaii</Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: "#CF3C7E" }}>
-          <Text style={{ fontSize: 0.2, textAlign: "center" }}>Texas</Text>
-        </View>
-      </View>;
+      </View>
+    );
   }
-};
+}
 
 class NestedMessage extends React.Component {
   constructor(props) {
@@ -110,8 +147,7 @@ class NestedMessage extends React.Component {
     return (
       <View>
         {showMessage ? (
-          <VrButton 
-            onClick={ ()=> this.handleClick()}>
+          <VrButton onClick={() => this.handleClick()}>
             <Text
               style={{
                 backgroundColor: "#777879",
@@ -123,24 +159,25 @@ class NestedMessage extends React.Component {
                 textAlign: "center",
                 textAlignVertical: "center",
                 transform: [{ translate: [0, 0, -3] }]
-              }}>
+              }}
+            >
               {this.state.message}
             </Text>
           </VrButton>
-        ):(
-          <Text></Text>
-        )}</View>
+        ) : (
+          <Text />
+        )}
+      </View>
     );
   }
   handleClick() {
     this.setState({ message: "you just clicked me >.0 " });
   }
-  componentWillMount(){
+  componentWillMount() {
     console.log("will mount");
   }
-  componentDidMount(){
+  componentDidMount() {
     console.log("Did mount");
     // this.setState({ showMessage: false });
-    
   }
-};
+}
